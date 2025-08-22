87a67e9d06963b5ddcca5bb7e77cd922ab529d10
Seraj-Omar <srag20062017@gmail.com>
2025-08-22 18:44:39 +0300

    solution for the OOP assignment

---
+++ SerajOmar/script.js
@@ -0,0 +1,18 @@
+function student(name,age,grade){
+    this.name=name;
+    this.age=age;
+    
+    let Grade=grade;
+    this.getGrade=function(){
+        return Grade;
+    };
+    this.setGrade=function(newGrade){
+        if(newGrade==='A'||newGrade==='F')
+            Grade=newGrade;
+        else
+            console.log('invalid grade');
+    };
+    this.introduce=function(){
+        console.log(`Hi, my name is ${this.name}, I’m ${this.age} years old, and I’m in grade ${Grade}.`);
+    };
+}
