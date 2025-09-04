45b66dd5568a9bda9771d962c47a9b9383140970
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2025-09-04 14:38:37 +0300

    Update script.js
    validated the grade and made the variable name better

--- SerajOmar/script.js
+++ SerajOmar/script.js
@@ -1,21 +1,36 @@
-function student(name,age,grade){
-    this.name=name;
-    this.age=age;
-    
-    let Grade=grade;
-    this.getGrade=function(){
-        return Grade;
+function Student(name, age, grade) {
+    this.name = name;
+    this.age = age;
+
+    let _grade;
+
+    const isValid = (g) => g === 'A' || g === 'F';
+
+    if (isValid(grade))
+        _grade = grade;
+    else
+        console.log("Invalid grade");
+        _grade = 'F';
+
+    this.getGrade = function () {
+        return _grade;
     };
-    this.setGrade=function(newGrade){
-        if(newGrade==='A'||newGrade==='F')
-            Grade=newGrade;
+
+    this.setGrade = function (newGrade) {
+        if (isValid(newGrade))
+            _grade = newGrade;
         else
-            console.log('invalid grade');
+            console.log("Invalid grade");
     };
-    this.introduce=function(){
-        console.log(`Hi, my name is ${this.name}, I’m ${this.age} years old, and I’m in grade ${Grade}.`);
+
+    this.introduce = function () {
+        console.log(`Hi, my name is ${this.name}, I’m ${this.age} years old, and I’m in grade ${_grade}.`);
     };
 }
 
-let studentObj=new student('Ali',20,'A');
+let studentObj = new Student('Ali', 20, 'A');
+studentObj.introduce();
+
+studentObj.setGrade('B');
+studentObj.setGrade('F');
 studentObj.introduce();
